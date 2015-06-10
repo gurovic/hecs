7d1e397cdf2eb201568165a6f40f9f8a8cccdd31
vladimir gurovic <gurovic@gmail.com>
2015-06-10 07:58:48 +0300

    hexagon made with borders

--- hecstopics.py
+++ hecstopics.py
@@ -31,25 +31,19 @@ for row in csvreader:
         
 
         
-fout = open("index_gen.html", "w", encoding="utf-8")
+fout = open("index.html", "w", encoding="utf-8")
 
 
 print(HEADER, file=fout)
 
 for j in range(10, 24, 2):
-    print('''    <div class="hecs left">
-        <div class="text {}"><a href="{}">{}</a></div>
-    </div>
+    print('''    <div class="hecs left {}"><div class="inner"><a href="{}">{}</a></div></div>
 '''.format(color[j][8] or "", "pages/" + str(j) + "-8.html",text[j][8] or ""), file=fout)
     for i in range(9, 9 + 17):
-        print('''    <div class="hecs">
-        <div class="text {}"><a href="{}">{}</a></div>
-    </div>
+        print('''    <div class="hecs {}"><div class="inner"><a href="{}">{}</a></div></div>
 '''.format(color[j][i] or "", "pages/" + str(j) + "-" + str(i) + ".html",text[j][i] or ""), file=fout)
     for i in range(9, 9 + 17):
-        print('''    <div class="hecs">
-        <div class="text {}"><a href="{}">{}</a></div>
-    </div>
+        print('''    <div class="hecs {}"><div class="inner"><a href="{}">{}</a></div></div>
 '''.format(color[j + 1][i] or "", "pages/" + str(j + 1) + "-" + str(i) + ".html",text[j + 1][i] or ""), file=fout)
         
 print(FOOTER, file=fout)
